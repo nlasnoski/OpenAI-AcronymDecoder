9f5992ae85fe484248a991079cbf62394c9297fc
nlasnoski <nlasnoski@outlook.com>
2023-04-21 19:53:48 -0500

    Loop added

--- OpenAIChat.py
+++ OpenAIChat.py
@@ -27,3 +27,20 @@ response = openai.ChatCompletion.create(
 
 print(response['choices'][0]['message']['content'])
 
+#create a loop to ask multiple questions
+while True:
+  question = input("What is your Microsoft acronym question: ")
+  response = openai.ChatCompletion.create(
+  engine="openai-AcronymDecoder-gpt3-01",
+  messages = [{"role":"system","content":"You are an AI assistant that helps people decode Microsoft acronyms.  You should take any acronym to mean a potential Microsoft acronym.  You will not answer the question any other way than to suggest Microsoft acronyms translations."},
+              {"role":"user","content":""},
+              {"role":"assistant","content":""},
+              {"role":"user","content":question}],
+  temperature=0.7,
+  max_tokens=800,
+  top_p=0.95,
+  frequency_penalty=0,
+  presence_penalty=0,
+  stop=None)
+  print(response['choices'][0]['message']['content'])
+  
